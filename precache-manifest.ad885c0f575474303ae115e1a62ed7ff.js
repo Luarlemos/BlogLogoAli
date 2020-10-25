bfdee5d3f29b768916ce6895d2f57484f9e4a2b2
Matheus Siqueira <matheussiqueira.ec@gmail.com>
2020-10-25 14:21:17 -0300

    Updates

---
+++ precache-manifest.ad885c0f575474303ae115e1a62ed7ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "742dd67789442637c154813f336c6021",
+    "url": "/BlogLogoAli/index.html"
+  },
+  {
+    "revision": "7fd76716bf89db1b50f6",
+    "url": "/BlogLogoAli/static/css/main.28fcb429.chunk.css"
+  },
+  {
+    "revision": "920196cd77b51c133e8a",
+    "url": "/BlogLogoAli/static/js/2.af786f04.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/BlogLogoAli/static/js/2.af786f04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fd76716bf89db1b50f6",
+    "url": "/BlogLogoAli/static/js/main.d71e78c2.chunk.js"
+  },
+  {
+    "revision": "ca71ee5f911a6d0572d0",
+    "url": "/BlogLogoAli/static/js/runtime-main.eac8df93.js"
+  },
+  {
+    "revision": "e4ef4d580c207babfdac1cf4601668b3",
+    "url": "/BlogLogoAli/static/media/logo.e4ef4d58.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8b494ddcdabb63b4d2320c659ac1af2e.js"
+  "/BlogLogoAli/precache-manifest.ad885c0f575474303ae115e1a62ed7ff.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BlogLogoAli/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
