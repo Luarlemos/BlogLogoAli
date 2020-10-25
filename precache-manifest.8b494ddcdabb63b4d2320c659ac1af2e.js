c3fc3696df440b14934d114fae9a96071a48810b
Matheus Siqueira <matheussiqueira.ec@gmail.com>
2020-10-25 14:20:31 -0300

    Updates

---
+++ precache-manifest.8b494ddcdabb63b4d2320c659ac1af2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e860049bfa07a9c0619449f15182480f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0275980335fef12132ce",
+    "url": "/static/css/main.28fcb429.chunk.css"
+  },
+  {
+    "revision": "920196cd77b51c133e8a",
+    "url": "/static/js/2.af786f04.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.af786f04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0275980335fef12132ce",
+    "url": "/static/js/main.07052acf.chunk.js"
+  },
+  {
+    "revision": "0acf6def864690f0fe91",
+    "url": "/static/js/runtime-main.566cc003.js"
+  },
+  {
+    "revision": "e4ef4d580c207babfdac1cf4601668b3",
+    "url": "/static/media/logo.e4ef4d58.png"
+  }
+]);
